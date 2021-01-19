08023bb4c54bf110efe1731dd963c70d2bc32301
jSchulzG <jan.schulz-gericke@gmx.net>
2021-01-19 11:38:00 +0100

    Split out unit tests into two files

--- superlists/settings.py
+++ superlists/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lq=$%gb16p3i#bl!pjqt#jf0)q-7_ux4x^satrr4aw=q7q91fz'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
